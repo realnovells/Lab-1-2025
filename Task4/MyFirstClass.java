cdd21a946478908505cac90e52d47fc39b328d5e
Dima <garry37gg@gmail.com>
2025-09-21 13:16:56 +0400

    Задание 4: Таблица произведений

---
+++ Task4/MyFirstClass.java
@@ -0,0 +1,44 @@
+class MyFirstClass {
+	public static void main(String[] args) {
+	MySecondClass o = new MySecondClass(0,0);
+	int i,j;
+	for(i = 1; i <= 8; i++) {
+		for(j = 1; j <= 8; j++) {
+			o.setNumber1(i);
+			o.setNumber2(j);
+			System.out.print(o.multiplyNumbers() + " ");
+			System.out.print(" ");
+	}
+	System.out.println();
+	}
+    }
+}
+
+class MySecondClass {
+	private int Number1;
+	private int Number2;
+
+	public MySecondClass(int Number1, int Number2) {
+		this.Number1 = Number1;
+		this.Number2 = Number2;
+ }
+
+ public int getNumber1() {
+	return Number1;
+}
+
+ public int getNumber2() {
+	return Number2;
+}
+
+ public void setNumber1(int Number1) {
+	this.Number1 = Number1;
+}
+ public void setNumber2(int Number2) {
+	this.Number2 = Number2;
+}
+
+ public int multiplyNumbers() {
+	return Number1 * Number2;
+	}
+}
