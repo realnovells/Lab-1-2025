b15a5354536366b3dc8044842b891f9f16db22b1
Dima <garry37gg@gmail.com>
2025-09-28 13:50:39 +0400

    исправлены изменения

--- Task3/MyFirstProgram.java
+++ Task3/MyFirstProgram.java
@@ -1,8 +1,44 @@
-class MyFirstClass {
-	public static void main(String[] s) {
-		for(int i = 0; i < s.length; i++)
-			System.out.println(s[i]);
+class MyFirstProgram {
+	public static void main(String[] args) {
+	MySecondClass o = new MySecondClass(0,0);
+	int i,j;
+	for(int i = 1; i <= 8; i++) {
+		for(j = 1; j <= 8; j++) {
+			o.setFisrtNumber(i);
+			o.setSecondNumber(j);
+			System.out.print(o.multiplyNumbers());
+			System.out.print(" ")
+	}
+	System.out.println();
+	}
+    }
+}
 
+class MySecondClass {
+	private int Number1;
+	private int Number2;
 
+	public MySecondClass(int int Number1, int Number2) {
+		this.Number1 = Number1;
+		this.Number2 = Number2;
+ }
+
+ public int getNumber1() {
+	return Number1;
+}
+
+ public int getNumber2() {
+	return Number2;
+}
+
+ public void setNumber1(int Number1) {
+	this.Number1 = Number1;
+}
+ public void setNumber2(int Number2) {
+	this.Number2 = Number2;
+}
+
+ public int multiplyNumbers() {
+	return Number1 * Number2;
 	}
 }
